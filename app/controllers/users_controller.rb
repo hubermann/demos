e5b509e32733ff38ad151e086e1148cbf9a267cf
hubermann <hubermann@gmail.com>
2016-02-23 10:50:56 -0300

    antes de migracion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,20 @@ class UsersController < ApplicationController
 	def create
     @user = User.new(user_params) ## Invoke user_params method
     if @user.save
-      redirect_to @user, notice: 'User was successfully created.' 
+      #redirect_to @user, notice: 'User was successfully created.' 
+      render status:200, json: {
+				message: "User created",
+				user: @user
+			}.to_json
     else
-      render action: 'new'
+      render status:422, json: {
+				errors: @user.errors
+				}.to_json
     end       
   end
 
 
-  
+
 	private 
 	def user_params
       params.require(:user).permit(:username, :email, :password, :password_confirmation)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   EMAIL_REGEX = /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i
   validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
   validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
-  validates :password, :confirmation => true #password_confirmation attr
+  validates :password, :confirmation => true #password_confirmation
   validates_length_of :password, :in => 6..20, :on => :create
   before_save :encrypt_password
   after_save :clear_password
